2ed7a52c914f39475c113e81256d777f09f4ca37
a5-stable <sh07e1916@gmail.com>
2023-03-19 23:20:30 +0900

    access store by using useSelector

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+  // access to slice
+  const postList = useSelector((state) => state.posts.value)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import { configureStore } from "@reduxjs/toolkit"
 import { Provider } from 'react-redux';
+import postsReducer from "./features/Posts"
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    posts: postsReducer,
+  }
 })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
