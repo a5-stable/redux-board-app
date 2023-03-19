b1454e7eecc87a112849c078181201ef1fcab531
a5-stable <sh07e1916@gmail.com>
2023-03-19 23:14:03 +0900

    export reducer from slice

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+  {
+    id: 1,
+    name: "unknown",
+    content: "hello!",
+  },
+  {
+    id: 2,
+    name: "名無しさん",
+    content: "ok",
+  },
+  {
+    id: 3,
+    name: "駆け出しエンジニア",
+    content: "good",
+  },
+  {
+    id: 4,
+    name: "スタックオーバーフローさん",
+    content: "starbacks",
+  },
+];
