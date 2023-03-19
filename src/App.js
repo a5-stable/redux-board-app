19c4ef14122779fc50ca4d234f1fae73669bd17c
a5-stable <sh07e1916@gmail.com>
2023-03-19 23:28:44 +0900

    display posts to ui

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ function App() {
   const postList = useSelector((state) => state.posts.value)
   return (
     <div className="App">
+      <h1>掲示板</h1>
+      <div className="addPost">
+        <input type="text" placeholder='your name'/>
+        <input type="text" placeholder='your post'/>
+        <button>投稿</button>
+      </div>
+      <div className='displayPosts'>
+        {postList.map((post) => (
+          <>
+            <div key={post.id} className='post'>
+              <h1>{post.name}</h1>
+              <p className='postContent'>{post.content}</p>
+            </div>
+            <hr />
+          </>
+        ))}
+      </div>
     </div>
   );
 }
